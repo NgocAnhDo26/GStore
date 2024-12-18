56cbda9498ebcec4dda9d95844c942e5d062171f
NgocAnhDo26 <dotanngocanh@gmail.com>
2024-12-18 16:15:50 +0700

    Header & footer added

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import React from "react";
 import UserProfile from "./pages/user-profile";
 import Checkout from "./pages/checkout";
 import SupportPage from "./pages/support-page";
+import Header from "./components/partials/header"
+import Footer from "./components/partials/footer"
 
 function App() {
   return (
-    <Checkout />
+    <Header />
   );
 }
 
