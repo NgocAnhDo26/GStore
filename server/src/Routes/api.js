75511aa3e8f8e8bb0f28c4019675e859d1994d3e
PhamHoangKha1403 <phamhoangkha14032004@gmail.com>
2024-12-25 15:28:58 +0700

    fix bug route

--- server/src/Routes/api.js
+++ server/src/Routes/api.js
@@ -2,10 +2,8 @@ import purchaseController from "../components/cart/purchaseController.js"
 import wishlistController from "../components/wishlist/wishlistController.js"
 import reviewController from "../components/review/reviewController.js";
 import gameController from "../components/game/gameController.js";
-import * as collectionService from "../components/user/userService/collectionService.js";
-import * as historyService from "../components/user/userService/historyService.js";
-import * as reviewService from "../components/user/userService/reviewService.js";
-import * as wishlistService from "../components/user/userService/wishlistService.js";
+
+
 import cartController from "../components/cart/cartController.js";
 import { authorize } from "../components/auth/verifyRoute.js";
 import express from "express";
@@ -17,16 +15,6 @@ router.use("/review",reviewController);
 router.use("/checkout",purchaseController);
 router.use("/cart", authorize(), cartController);
 
-router.get("/history", (req, res) => {
-  historyService
-    .fetchHistoryWithQuery(req.user.id, req.query)
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((e) => {
-      console.log(e);
-      res.status(500).json(e);
-    });
-});
+
 
 export default router;
--- server/src/Routes/user.js
+++ server/src/Routes/user.js
@@ -1,26 +1,11 @@
 import feedbackController from "../components/user/userController/feedbackController.js";
-import * as accountController from "../components/user/userController/accountController.js";
-import * as collectionController from "../components/user/userController/collectionController.js";
-import * as reviewController from "../components/user/userController/reviewController.js";
-import * as historyController from "../components/user/userController/historyController.js";
-import * as wishlistController from "../components/user/userController/wishlistController.js";
+
 import { authorize } from "../components/auth/verifyRoute.js";
 import express from "express";
 
 const router = express.Router();
 
-router.get("/info", authorize(), accountController.getUserInfo);
-router.post("/info", authorize(), accountController.updateUserInfo);
 router.use("/feedback", authorize(), feedbackController);
-router.get(
-  "/collection",
-  authorize(),
-  collectionController.getUserGameCollection,
-);
-router.get("/history", authorize(), historyController.getPurchaseHistory);
 
-router.get("/review", authorize(), reviewController.getUserReview);
-
-router.get("/wishlist", authorize(), wishlistController.getUserWishlist);
 
 export default router;
